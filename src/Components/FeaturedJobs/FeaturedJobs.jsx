5f7b787d928ec6042fbdcf46c5139f68b7d9c708
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-15 16:53:00 +0600

    47-4 Set Page Layout And Display Featured Jobs done

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -17,7 +17,7 @@ const FeaturedJobs = () => {
                 <h2 className='text-5xl'>Featured Jobs : {jobs.length}</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima veritatis cum, aliquid magnam placeat iste aliquam, temporibus fugiat impedit natus, accusantium accusamus. Nisi natus aperiam eos id voluptatum temporibus sed hic, placeat ad laudantium consequatur. Facere vitae natus consequatur pariatur sit! Placeat ea, laborum sapiente nemo repellat excepturi! Nemo, assumenda.</p>
             </div>
-            <div className='grid grid-cols-2'>
+            <div className='grid grid-cols-2 gap-5'>
                 {
                     jobs.map((job)=><SingleJob key={job.id} job={job}></SingleJob>)
                 }
--- src/Components/SingleJob/SingleJob.jsx
+++ src/Components/SingleJob/SingleJob.jsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React from "react";
 //47-3 Load Jobs Data For The Page
+//47-4 Set Page Layout And Display Featured Jobs
 
-const SingleJob = ({job}) => {
-    console.log(job)
-    const {company_name,contact_information,logo} = job
-    console.log(company_name);
-    return (
-        <div>
-            {/* <h2>Hi</h2> */}
-            {/* <img src="" alt="" srcset="" /> */}
-            {/* <img src={logo} alt="" /> */}
+const SingleJob = ({ job }) => {
+  console.log(job);
+  const {
+    id,
+    job_title,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+    job_description,
+    job_responsibility,
+    educational_requirements,
+    experiences,
+    contact_information,
+    company_name,
+    logo,
+  } = job;
+  console.log(company_name);
+  return (
+    <div>
+      {/* <h2>Hi</h2> */}
+      {/* <img src="" alt="" srcset="" /> */}
+      {/* <img src={logo} alt="" /> */}
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src={logo} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-
+      <div className="card  bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={logo} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body  text-center">
+          <h2 className="card-title">{job_title}</h2>
+          <p>{company_name}</p>
+          <div>
+            <button className="px-5 py-2 font-extrabold border rounded border-sky-800 mr-4 text-blue-600">
+              {remote_or_onsite}
+            </button>
+            <button className="px-5 py-2 font-extrabold border rounded border-sky-800 mr-4 text-blue-600">
+              {job_type}
+            </button>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-primary">View Details</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleJob;

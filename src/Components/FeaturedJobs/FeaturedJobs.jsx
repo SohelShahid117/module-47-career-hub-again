27c63d11755b29b5164277373dceaad5c1de277f
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-15 19:04:46 +0600

    47-5 Add Icons And Handle Show All Jobs code done

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,31 +1,51 @@
-import React, { useState,useEffect } from 'react';
-import SingleJob from '../SingleJob/SingleJob';
+import React, { useState, useEffect } from "react";
+import SingleJob from "../SingleJob/SingleJob";
 
 const FeaturedJobs = () => {
-    const [jobs,setJobs] = useState([])
+  const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
-    useEffect(() => {
-        fetch('jobs.json')
-            .then((res)=>res.json())
-            // .then((data)=>console.log(data))
-         .then((data)=>setJobs(data))
-    },[]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      // .then((data)=>console.log(data))
+      .then((data) => setJobs(data));
+  }, []);
 
-    return (
-        <div>
-            <div className='text-center'>
-                <h2 className='text-5xl'>Featured Jobs : {jobs.length}</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima veritatis cum, aliquid magnam placeat iste aliquam, temporibus fugiat impedit natus, accusantium accusamus. Nisi natus aperiam eos id voluptatum temporibus sed hic, placeat ad laudantium consequatur. Facere vitae natus consequatur pariatur sit! Placeat ea, laborum sapiente nemo repellat excepturi! Nemo, assumenda.</p>
-            </div>
-            <div className='grid grid-cols-2 gap-5'>
-                {
-                    jobs.map((job)=><SingleJob key={job.id} job={job}></SingleJob>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className="text-5xl">Featured Jobs : {jobs.length}</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+          veritatis cum, aliquid magnam placeat iste aliquam, temporibus fugiat
+          impedit natus, accusantium accusamus. Nisi natus aperiam eos id
+          voluptatum temporibus sed hic, placeat ad laudantium consequatur.
+          Facere vitae natus consequatur pariatur sit! Placeat ea, laborum
+          sapiente nemo repellat excepturi! Nemo, assumenda.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-5">
+        {/* {jobs.map((job) => (
+          <SingleJob key={job.id} job={job}></SingleJob>
+        ))} */}
+        {jobs.slice(0, dataLength).map((job) => (
+          <SingleJob key={job.id} job={job}></SingleJob>
+        ))}
+      </div>
+      {/* <div className={dataLength === jobs.length && "hidden"}> */}
+      <div className={dataLength === jobs.length ? "hidden" : ""}>
+        <button
+          onClick={() => {
+            setDataLength(jobs.length);
+          }}
+          className="btn btn-primary"
+        >
+          Show All Jobs
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedJobs;
-
-//47-4 Set Page Layout And Display Featured Jobs---6 min teke soro hbe
--- src/Components/SingleJob/SingleJob.jsx
+++ src/Components/SingleJob/SingleJob.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { BsCurrencyDollar } from "react-icons/bs";
 //47-3 Load Jobs Data For The Page
 //47-4 Set Page Layout And Display Featured Jobs
 
@@ -40,6 +42,16 @@ const SingleJob = ({ job }) => {
             <button className="px-5 py-2 font-extrabold border rounded border-sky-800 mr-4 text-blue-600">
               {job_type}
             </button>
+          </div>
+          <div className="flex">
+            <h2 className="text-2xl flex items-center mt-4 mr-6">
+              <MdOutlineLocationOn className="mr-1" />
+              {location}
+            </h2>
+            <h2 className="text-2xl flex items-center mt-4 ">
+              <BsCurrencyDollar className="mr-1" />
+              <span>Salary: {salary} </span>
+            </h2>
           </div>
           <div className="card-actions">
             <button className="btn btn-primary">View Details</button>
